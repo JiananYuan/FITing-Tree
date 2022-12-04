05b5ef7d7bb2efa4f10abf45b0deb406e96552b1
JiananYuan <yuanjiajiaqq@163.com>
2022-12-04 16:01:54 +0800

    feat: exp ready

--- exp.sh
+++ exp.sh
@@ -4,34 +4,34 @@ dataset=(linear seg1 seg10 normal books_200M_uint64 fb_200M_uint64 osmc_200M_uin
 for ds in ${dataset[@]}
 do
     echo ">>>>>>>>>> $ds: 时延 <<<<<<<<<<"
-    ../build/main_latency ../dataset/$ds.csv 0 1
+    ./build/main_latency ./dataset/$ds.csv 0 1
 done
 
 for ds in ${dataset[@]}
 do
     echo ">>>>>>>>>> $ds: 不同静态数据集吞吐量 <<<<<<<<<<"
-    ../build/main_goodput ../dataset/$ds.csv 0 1
+    ./build/main_goodput ./dataset/$ds.csv 0 1
 done
 
 # 读多写少
 for ds in ${dataset[@]}
 do
     echo ">>>>>>>>>> $ds: 读多写少负载的吞吐量 <<<<<<<<<<"
-    ../build/main_workload ../dataset/$ds.csv 0 1 0.8 0.2
+    ./build/main_workload ./dataset/$ds.csv 0 1 0.8 0.2
 done
 
 # 写多读少
 for ds in ${dataset[@]}
 do
     echo ">>>>>>>>>> $ds: 写多读少负载的吞吐量 <<<<<<<<<<"
-    ../build/main_workload ../dataset/$ds.csv 0 1 0.2 0.8
+    ./build/main_workload ./dataset/$ds.csv 0 1 0.2 0.8
 done
 
 # 读写均衡
 for ds in ${dataset[@]}
 do
     echo ">>>>>>>>>> $ds: 读写均衡负载的吞吐量 <<<<<<<<<<"
-    ../build/main_workload ../dataset/$ds.csv 0 1 0.5 0.5
+    ./build/main_workload ./dataset/$ds.csv 0 1 0.5 0.5
 done
 
 # 实验准备
